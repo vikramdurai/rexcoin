446674cdfddbe228e0a308cbd86053c13de444f7
Vikram Durai <vikramvdurai@gmail.com>
2018-03-02 14:13:48 +0530

    version one

---
+++ main.py
@@ -0,0 +1,15 @@
+class Block:
+    def __init__(self):
+        pass
+
+class Blockchain:
+    def __init__(self):
+        pass
+    def registerTx(self, tx):
+        pass
+
+class Address:
+    def __init__(self):
+        pass
+    def pay(self, otheraddress):
+        pass
